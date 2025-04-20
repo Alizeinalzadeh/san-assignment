1d176f468bb548a524d66839f2b9e53d5582d0d6
alizeinalzadeh <ali94zeinalzadeh@gmail.com>
2025-04-20 20:33:29 +0330

    feat(app): enhance App component with user authentication and routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
-function App() {
-    return <></>
-}
+import AppRoutes from './components/Share/Router/AppRoutes/AppRoutes';
+import Header from './components/Layout/Header/Header';
+import { useUser } from './store/auth';
 
-export default App
+export default function App() {
+	const { data: user } = useUser();
+
+	return (
+		<>
+			{user && <Header />}
+			<AppRoutes />
+		</>
+	);
+}
